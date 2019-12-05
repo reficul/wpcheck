39b261a23b3f39649fb943246a8c234f5b77b743
Gabriele Venturini <iotivedo+github@gmail.com>
2019-12-05 14:53:43 +0100

    Adding Colors

--- wpcheck.sh
+++ wpcheck.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 clear
 
+#COLORS
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 read -r -p "Are you sure you want to check all wordpress installed on server? [y/N] " response
 ucheck=http://api.wordpress.org/core/stable-check/1.0/
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
@@ -12,7 +29,13 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
                         if [ -f "$d/public_html/wp-config.php" ]; then
                                 ver='"'$(wp core version --path=$d/public_html --allow-root)'"'
                                 status=$(curl -s "$ucheck" | egrep -m 1 "$ver" | awk -F '"' '{ print $4 }')
-                                printf "%-20s | %-10s | %-10s\n" "$d" "$ver" "$status"
+                                if [ "$status" == "insecure" ]; then
+                                        printf "%-20s | %-10s | %-10s\n" "$d" "$ver" "${RED}$status${NORMAL}"
+                                elif [ "$status" == "latest" ]; then
+                                        printf "%-20s | %-10s | %-10s\n" "$d" "$ver" "${GREEN}$status${NORMAL}"
+                                else
+                                    	printf "%-20s | %-10s | %-10s\n" "$d" "$ver" "$status"
+                                fi
                         fi
                 fi
         done
